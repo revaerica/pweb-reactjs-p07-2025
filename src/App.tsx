8f2f8f2ab9396ae85170aaa4e83fae07f54002db
reva <reva.erica24@gmail.com>
2025-11-08 00:06:21 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,79 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import Navbar from './components/Navbar';
 
+import Login from './pages/Login';
+import Register from './pages/Register';
+import BooksList from './pages/BooksList';
+import BookDetail from './pages/BookDetail';
+import AddBook from './pages/AddBook';
+import Transactions from './pages/Transactions';
+import TransactionDetail from './pages/TransactionDetail';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <div className="app">
+          <Navbar />
+          <main className="main-content">
+            <Routes>
+              {/* Public Routes */}
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+
+              {/* Protected Routes */}
+              <Route
+                path="/books"
+                element={
+                  <ProtectedRoute>
+                    <BooksList />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/books/:id"
+                element={
+                  <ProtectedRoute>
+                    <BookDetail />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/books/add"
+                element={
+                  <ProtectedRoute>
+                    <AddBook />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/transactions"
+                element={
+                  <ProtectedRoute>
+                    <Transactions />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/transactions/:id"
+                element={
+                  <ProtectedRoute>
+                    <TransactionDetail />
+                  </ProtectedRoute>
+                }
+              />
+
+              {/* Default Route */}
+              <Route path="/" element={<Navigate to="/books" replace />} />
+              <Route path="*" element={<Navigate to="/books" replace />} />
+            </Routes>
+          </main>
+        </div>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
