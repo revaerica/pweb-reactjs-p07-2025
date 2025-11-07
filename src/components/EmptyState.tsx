38fbd8e639cfe4b54ea813c6b0153e94015a4b82
reva <reva.erica24@gmail.com>
2025-11-07 23:57:29 +0700

    Update EmptyState.tsx

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -1,1 +1,18 @@
+interface EmptyStateProps {
+  message?: string;
+  icon?: string;
+}
 
+const EmptyState: React.FC<EmptyStateProps> = ({
+  message = 'No data available',
+  icon = '📭',
+}) => {
+  return (
+    <div className="empty-container">
+      <div className="empty-icon">{icon}</div>
+      <p className="empty-message">{message}</p>
+    </div>
+  );
+};
+
+export default EmptyState;
