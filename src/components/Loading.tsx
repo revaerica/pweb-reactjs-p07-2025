4dddabfb2ea7bafb336954aeebaa3e10df31dea4
reva <reva.erica24@gmail.com>
2025-11-07 23:58:59 +0700

    Update Loading.tsx

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,1 +1,14 @@
+interface LoadingProps {
+  message?: string;
+}
 
+const Loading: React.FC<LoadingProps> = ({ message = 'Loading...' }) => {
+  return (
+    <div className="loading-container">
+      <div className="spinner"></div>
+      <p className="loading-message">{message}</p>
+    </div>
+  );
+};
+
+export default Loading;
