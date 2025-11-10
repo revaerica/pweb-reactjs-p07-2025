1e02df7f2823f2a0f66c5a013a714c3f13b230ba
reva <reva.erica24@gmail.com>
2025-11-10 12:58:55 +0700

    Update transactions.ts

--- src/services/transactions.ts
+++ src/services/transactions.ts
@@ -1,27 +1,24 @@
 import api from './api';
-import { Transaction, CreateTransactionData, ApiResponse, TransactionFilters } from '../types';
+import { ApiResponse, Order, CreateOrderData } from '../types';
 
 export const transactionsService = {
-  async getTransactions(filters?: TransactionFilters): Promise<ApiResponse<Transaction[]>> {
-    const params = new URLSearchParams();
-    
-    if (filters?.search) params.append('search', filters.search);
-    if (filters?.sort_by) params.append('sort_by', filters.sort_by);
-    if (filters?.order) params.append('order', filters.order);
-    if (filters?.page) params.append('page', filters.page.toString());
-    if (filters?.per_page) params.append('per_page', filters.per_page.toString());
-
-    const response = await api.get<ApiResponse<Transaction[]>>(`/transactions?${params.toString()}`);
-    return response.data;
+  async getTransactions() {
+    const res = await api.get<ApiResponse<Order[]>>('/transactions');
+    return res.data.data;
   },
 
-  async getTransaction(id: number): Promise<Transaction> {
-    const response = await api.get<{ data: Transaction }>(`/transactions/${id}`);
-    return response.data.data;
+  async getTransaction(id: string) {
+    const res = await api.get<ApiResponse<Order>>(`/transactions/${id}`);
+    return res.data.data;
   },
 
-  async createTransaction(data: CreateTransactionData): Promise<Transaction> {
-    const response = await api.post<{ data: Transaction }>('/transactions', data);
-    return response.data.data;
+  async createTransaction(payload: CreateOrderData) {
+    const res = await api.post<ApiResponse<Order>>('/transactions', payload);
+    return res.data.data;
+  },
+
+  async getStatistics() {
+    const res = await api.get<ApiResponse<any>>('/transactions/statistics');
+    return res.data.data;
   },
 };
